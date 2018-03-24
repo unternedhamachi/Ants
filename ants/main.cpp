3544213ec075433389b4878083543aa57d1c37f2
Kevin Faulhaber <kevin.faulhaber@tuta.io>
2018-03-24 22:00:30 +0100

    Initial Commit

---
+++ ants/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ants
+//
+//  Created by Kevin Faulhaber on 24/03/2018.
+//  Copyright © 2018 Julia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
